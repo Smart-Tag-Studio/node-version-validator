389cdf957e4a2a135bbcf5d2d5154cb16e38ca29
Cesar Costas <costascarrera.cesar@gmail.com>
2021-05-14 11:17:11 +0100

    Make it executable as nvv and add some documentation

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
 # node-version-validator
 
+## Install
+
 Install with `npm i --save github:Smart-Tag-Studio/node-version-validator`
 
-Simply import or require it on a js file
+## Use
+
+You can use it in two ways:
+1. Simply import or require it on a js file
 ```js
 require('node-version-validator');
 ```
+
+2. Add it to your npm script using `nvv` (node-version-validator), for example:
+```json
+"scripts": {
+    "dev": "nvv && node ./scripts/dev.mjs",
+}
+```
+
+## The problem
+
+Read: https://medium.com/codeptivesolutions/want-to-check-node-version-before-project-get-executed-using-script-47cd32c2f1fe
+
+## The solution
+
+It tests that when you write:
+```json
+"engines": {
+    "node": ">=17.0.0"
+},
+"engineStrict": true,
+```
+in the package json, it is respected when executing a script.
+`engineStrict` will change the script to give an error (when true) to just a warning (when false or not used).
+
+This is an example of the output:
+```
+$ npm run dev
+
+> @my-scope/my-project@1.0.0 dev
+> nvv && ./scripts/dev.mjs
+
+You're currently running node 16.0.0. This project requires node >=17.0.0.
+Change your node version and try again
+```
+
+What it does is to compare `process.versions` against the definition in the `package.json`.
--- index.js
+++ index.js
@@ -1,44 +1,53 @@
+#!/usr/bin/env node
 const semver = require('semver');
 const pkg_json = process.env[`npm_package_json`];
 const pkg_name = process.env[`npm_package_name`];
 let pkg;
 
-if (!pkg_name) {
-    console.log(`Run this script inside an npm script to receive the npm environmental variables`);
-    process.exit(1);
-}
-
-if (pkg_json) {
-    pkg = JSON.parse(require('fs').readFileSync(pkg_json, 'UTF-8'));
-} else {
-    pkg = {
-        engineStrict: !!process.env[`npm_package_engineStrict`],
-        engines: Object.keys(process.env).filter(k => k.match(`npm_package_engines_`)).reduce((s,k) => { s[k.replace(`npm_package_engines_`, '')]=process.env[k]; return s; }, {}),
-    };
-}
-
-if (!pkg.engines) {
-    process.exit(0);
-}
-
-Object.keys(process.versions).forEach((engine) => {
-    const version = {
-        required: pkg.engines[engine],
-        current: process.versions[engine],
-    };
-
-    let matches;
-    if (version.required && version.current) {
-        matches = semver.satisfies(version.current, version.required);
+try {
+    if (!pkg_name) {
+        console.log(`Run this script inside an npm script to receive the npm environmental variables`);
+        process.exit(1);
     }
-    
-    if (matches === false) {
-        if (pkg.engineStrict) {
-            console.log(`You're currently running ${engine} ${version.current}. This project requires ${engine} ${version.required}.`);
-            console.log(`Change your ${engine} version and try again`);
-            process.exit(1);
+
+    if (pkg_json) {
+        pkg = JSON.parse(require('fs').readFileSync(pkg_json, 'UTF-8'));
+    } else {
+        pkg = {
+            engineStrict: !!process.env[`npm_package_engineStrict`],
+            engines: Object.keys(process.env).filter(k => k.match(`npm_package_engines_`)).reduce((s,k) => { s[k.replace(`npm_package_engines_`, '')]=process.env[k]; return s; }, {}),
+        };
+    }
+
+    if (!pkg.engines) {
+        process.exit(0);
+    }
+
+    Object.keys(pkg.engines).forEach((engine) => {
+        const version = {
+            required: pkg.engines[engine],
+            current: process.versions[engine],
+        };
+
+        let matches;
+        if (version.required && version.current) {
+            matches = semver.satisfies(version.current, version.required);
         } else {
-            console.log(`You're currently running ${engine} ${version.current}. This project recommends ${engine} ${version.required}.`);
+            console.error(`Unable to test ${engine} ${version.required}. Please open an issue on github: https://github.com/Smart-Tag-Studio/node-version-validator/issues`)
         }
-    }
-});
+        
+        if (matches === false) {
+            if (pkg.engineStrict) {
+                console.log(`You're currently running ${engine} ${version.current}. This project requires ${engine} ${version.required}.`);
+                console.log(`Change your ${engine} version and try again`);
+                process.exit(1);
+            } else {
+                console.log(`You're currently running ${engine} ${version.current}. This project recommends ${engine} ${version.required}.`);
+            }
+        }
+    });
+} catch(e) {
+    console.error(`\x1b[31m[node-version-validator] Something went wrong. ERROR STARTS HERE`);
+    console.error(e);
+    console.error(`\x1b[31m[node-version-validator] Something went wrong. ERROR ENDS HERE.\x1b[33m Please open an issue on github: https://github.com/Smart-Tag-Studio/node-version-validator/issues and send us the error above\x1b[37m`);
+}
